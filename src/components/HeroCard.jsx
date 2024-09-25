1fc63c9dfcb80f0ce3db65d19ea5999cc4693c97
Carol Li <dinosaur0514@gmail.com>
2024-09-25 22:10:37 +0800

    style: HeroCard 修正 Img 樣式，新增 CardBody 樣式

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,11 +1,6 @@
 import { styled } from "styled-components";
 import ImgSrc from "../assets/happyDoggie.png";
 
-const CardTitle = styled.h3`
-  font-size: 1.75rem;
-  text-align: center;
-`;
-
 const ImgWrapper = styled.div`
   box-shadow: 0 7px 17px -8px rgba(0, 0, 0, 0.8);
   transform: translate3d(0) scale(1);
@@ -13,17 +8,29 @@ const ImgWrapper = styled.div`
   transform: translateZ(0);
 `;
 
-// const Img = styled.image.attrs({
-//   src: `${ImgSrc}`,
-// })`
-//   display: block;
-//   margin: 0;
-//   object-fit: cover;
-//   object-position: center center;
-//   padding: 0;
-//   width: 100%;
-//   height: 20vh;
-// `;
+const Img = styled.img.attrs({
+  src: `${ImgSrc}`,
+  alt: 'Hero Image'
+})`
+  display: block;
+  margin: 0;
+  object-fit: cover;
+  object-position: center center;
+  padding: 0;
+  width: 100%;
+  height: 20vh;
+`;
+
+const CardBody = styled.div`
+  padding: 0.75rem 0;
+`;
+
+const CardTitle = styled.h3`
+  font-size: 1.75rem;
+  font-weight: 700;
+  text-align: center;
+  margin: 0;
+`;
 
 const Card = styled.div`
   padding-bottom: 0.5rem;
@@ -42,20 +49,11 @@ export default function HeroCard() {
   return (
     <Card>
       <ImgWrapper>
-        {/* <Img /> */}
-        <img src={ImgSrc} style={
-          {
-            display: 'block',
-            margin: 0,
-            'object-fit': 'cover',
-            'object-position': 'center center',
-            padding: 0,
-            width: '100%',
-            height: '20vh',
-          }
-        } alt="" />
+        <Img />
       </ImgWrapper>
-      <CardTitle>123</CardTitle>
+      <CardBody>
+        <CardTitle>Card Title</CardTitle>
+      </CardBody>
     </Card>
   );
 }
