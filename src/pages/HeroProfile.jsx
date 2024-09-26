81a5167de360cea70b1aef551222234ea0a85c28
Carol Li <dinosaur0514@gmail.com>
2024-09-27 04:20:01 +0800

    新增 HeroProfile 元件架構

--- src/pages/HeroProfile.jsx
+++ src/pages/HeroProfile.jsx
@@ -1,26 +1,49 @@
 import { styled } from "styled-components";
-import Border from "../components/Border";
+import Border from "../components/UI/Border";
+import Ability from "../components/Ability";
+import SubmitButton from "../components/SubmitButton";
 
-const ProfileWrapper = styled.div`
+const Wrapper = styled.div`
   width: 100%;
-  padding-top: 30px;
+  margin-top: 30px;
 `;
 
-const ProfileContent = styled.div`
-  padding: 0 3rem;
+const Content = styled.div`
+  margin: 0 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Ability = styled.div`
+const Footer = styled.div`
+  display: flex;
+  margin-top: 0.5rem;
+  justify-content: center;
 `;
 
-export default function HeroProfile() {
+const RemainingPoints = styled.p`
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 1.25rem;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+`;
+
+export default function HeroProfile({ remainingPoints }) {
   return (
-    <ProfileWrapper>
+    <Wrapper>
       <Border position="top" />
-      <ProfileContent>
-        <Ability>HeroProfile</Ability>
-      </ProfileContent>
+      <Content>
+        <RemainingPoints>剩餘分數：30{remainingPoints}</RemainingPoints>
+        <Ability />
+        <Ability />
+        <Ability />
+        <Ability />
+      </Content>
+      <Footer>
+        <SubmitButton />
+      </Footer>
       <Border position="bottom" />
-    </ProfileWrapper>
+    </Wrapper>
   );
 }
