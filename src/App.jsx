d9f8bca0d065cde662c7cbd186b911f5e773f08f
Carol Li <dinosaur0514@gmail.com>
2024-09-30 15:52:54 +0800

    /heroes 路由新增 loader，載入頁面前取得 Heroes API 資料

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HeroList from "./pages/HeroList"
 import HeroProfile from "./pages/HeroProfile";
+import getHeroes from "./api/getHeroes";
 
 const router = createBrowserRouter([
   {
     path: "/heroes",
     element: <HeroList />,
+    loader: getHeroes,
   },
   {
     path: "/heroes/:heroId",
