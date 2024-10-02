c72fa63df5552acd3f8689b52962023d036f239f
Carol Li <dinosaur0514@gmail.com>
2024-10-03 03:20:04 +0800

    新增 updateProfile API

--- src/api/apis.js
+++ src/api/apis.js
@@ -9,3 +9,17 @@ export async function getProfile(heroId) {
   );
   return response;
 }
+
+export async function updateProfile(heroId, method, heroData) {
+  const response = await fetch(
+    `https://hahow-recruit.herokuapp.com/heroes/${heroId}/profile`,
+    {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(heroData),
+    }
+  );
+  return response;
+}
