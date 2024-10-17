a8f8eb6e701a1f306142c5186c837de36153109f
Carol Li <dinosaur0514@gmail.com>
2024-10-17 18:37:57 +0800

    新增 Blank 類型通知的 icon，其他類型的 icon 依舊

--- src/utils/Notify.jsx
+++ src/utils/Notify.jsx
@@ -1,22 +1,30 @@
 import { Toaster } from "react-hot-toast";
 
 const toastOption = {
+  duration: 3000,
+  icon: "😎",
+  style: {
+    border: "2px solid #151515",
+  },
   success: {
     style: {
       background: "#e2ffdb",
       border: "2px solid #61d345",
     },
+    icon: undefined,
   },
   error: {
     style: {
       background: "#ffd7d7",
       border: "2px solid #ff4b4b",
     },
+    icon: undefined,
   },
   loading: {
     style: {
       border: "2px solid #ccc9c6",
     },
+    icon: undefined,
   },
 };
 
